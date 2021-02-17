4da099cfd91499ec3c7bcf276c2f6dd8982d091c
Matt Payne <Payne@MattPayne.org>
2021-02-16 19:59:46 -0600

    Demo for Jay - only print components with 5 or more nodes.

--- print_connected_components.py
+++ print_connected_components.py
@@ -1,10 +1,12 @@
 import networkx
 import pydot
-eg1 = pydot.graph_from_dot_file("eg1.dot")
-eg1[0].write_png("eg1.png")
+eg1 = pydot.graph_from_dot_file("eg2.dot")
+eg1[0].write_png("eg2.png")
 nxeg1 = networkx.drawing.nx_pydot.from_pydot(eg1[0])
 u1 = networkx.DiGraph.to_undirected(nxeg1)
 cc1=networkx.algorithms.connected_components(u1)
+
 for i, f in enumerate(cc1, start=1):
-  print("Component %d: %s" % (i, f))
+   if (len(f) >= 5): print("Component %d: %s" % (i, f))
+
 
